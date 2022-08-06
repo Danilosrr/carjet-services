de481c32b5351c0b2d227b8f2918bc28557ee90c
Danilo <danilo_srr@hotmail.com>
2022-08-06 02:37:59 -0300

    logout button only on map page

--- carjet-front/src/components/Map.js
+++ carjet-front/src/components/Map.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { Box, Button, Divider } from '@mui/material';
+import { FaHandshake } from "react-icons/fa";
+import { Oval } from "react-loader-spinner";
 import styled from 'styled-components';
 import Pin from './Pin';
-import { Box, Button, Divider } from '@mui/material';
 import StoreMallDirectoryIcon from '@mui/icons-material/StoreMallDirectory';
-import { FaHandshake } from "react-icons/fa";
 import useAuth from '../hooks/useAuth';
 import api from '../services/api';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from 'react-router-dom';
 
 const containerStyle = {
     position: 'relative',
     width: '100%',
     height: '100%'
 };
-
+const logoutbutton = { 
+    display: 'flex', 
+    justifyContent:'flex-start', 
+    backgroundColor: "rgba(255,255,255,0.95)", 
+    position: "absolute", 
+    bottom: "15px", 
+    right: "14px" 
+}
 const mapOptions = {
     zoomControl:false,
     fullscreenControl:false,
@@ -26,12 +36,18 @@ export default function Map(props) {
     const [providerBtn,setProviderBtn] = useState(false);
     const [branches,setBranches] = useState(null);
     const [providers,setProviders] = useState(null);
-    const { token } = useAuth();
-    
+    const { token,signOut } = useAuth();
+    const navigate = useNavigate();
+
     const { isLoaded } = useJsApiLoader({
         id: 'google-map',
         googleMapsApiKey: process.env.REACT_APP_MAP_KEY
     })
+
+    function handleSignOut() {
+        navigate("/");
+        signOut();
+    }
 
     useEffect(()=>{        
         function getCoords(geolocation) {
@@ -79,8 +95,11 @@ export default function Map(props) {
                 <Divider sx={{width: '100%', color: '#000000'}}/>
                 <Button sx={{ display:'flex', justifyContent:'flex-start', color:'#000000' }} fullWidth={true} startIcon={<StoreMallDirectoryIcon/>} onClick={()=>setBranchBtn(!branchBtn)}>Filiais</Button>
             </Box>
+            <Box sx={logoutbutton}>
+                <Button sx={{ color:"#000000" }} onClick={handleSignOut} startIcon={<LogoutIcon sx={{ fontSize: "26px"}}/>}>Sair</Button>
+            </Box>
         </MapStyle>
-    ) : <></>
+    ) : <Oval wrapperStyle={{position:"absolute", top:"0", bottom:"0", left:"0", right:"0", display:"flex", justifyContent:"center", alignItems:"center" }} type="Oval" color="#3cbeff" height={80} width={80}/>    
 }
 
 const MapStyle = styled.div`
